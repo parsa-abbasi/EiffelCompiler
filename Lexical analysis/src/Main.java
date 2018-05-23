af7f467233be2b72c8ada82b7271ae7168ea8c3f
Parsa Abasi <parsa.aba30@gmail.com>
2018-05-23 14:27:28 +0430

    Update Main.java
    Add a comment

--- Lexical analysis/src/Main.java
+++ Lexical analysis/src/Main.java
@@ -6,5 +6,5 @@ public class Main {
 		String currentDir = System.getProperty("user.dir");
 		jflex.Main.generate(new File(currentDir + "\\src" + "\\Eiffel.flex"));
 	}
-
+	//This function get the flex file and make it's java class using JFlex library
 }
